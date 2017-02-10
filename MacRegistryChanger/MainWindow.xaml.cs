7b8a4b6d5e37ca9a2766005ea3eee1358aa9b7e2
jankostrzewa <jankostrzewa@users.noreply.github.com>
2017-02-10 22:37:25 +0100

    Add project files.

---
+++ MacRegistryChanger/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace MacRegistryChanger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RegistryManipulation registryManipulation;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            registryManipulation = new RegistryManipulation();
+        }
+
+        private void btnGetAddress(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnIsCustomEnabled_Click(object sender, RoutedEventArgs e)
+        {
+            if (registryManipulation.IsCustomMacKeyEnabled())
+            {
+                lblCustomMacEnabled.Content = "Enabled!";
+                btnCreateCustomMac.IsEnabled = false;
+                btnChangeCustomMac.IsEnabled = true;
+                btnDeleteCustomMac.IsEnabled = true;
+            }
+            else
+            {
+                lblCustomMacEnabled.Content = "Disabled!";
+                btnCreateCustomMac.IsEnabled = true;
+                btnChangeCustomMac.IsEnabled = false;
+                btnDeleteCustomMac.IsEnabled = false;
+            }
+        }
+
+        private void btnCreateCustomMac_Click(object sender, RoutedEventArgs e)
+        {
+            string newMac = txtNewMAC.Text;
+            if (newMac.Length < 12)
+            {
+                MessageBox.Show("A new MAC address must be given!");
+                return;
+            }
+            newMac = newMac.Substring(0, 12);
+            registryManipulation.CreateCustomMacKey(newMac);
+        }
+
+        private void btnChangeCustomMac_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDeleteCustomMac_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSubkeys_Click(object sender, RoutedEventArgs e)
+        {
+            RegistryKey y = registryManipulation.GetWlanRegistryKey();
+            string a = y.Name;
+        }
+    }
+}
---
+++ MacRegistryChanger/RegistryManipulation.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace MacRegistryChanger
+{
+    class RegistryManipulation
+    {
+        private RegistryKey registryKey;
+        private string nwAdr;
+
+        public RegistryManipulation()
+        {
+            try
+            {
+                registryKey = GetWlanRegistryKey();
+            }
+            catch(InvalidOperationException e)
+            {
+                MessageBox.Show("No wireless card found!");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("Unable to modify registry - please run as an administrator!");
+            }
+            finally
+            {
+                nwAdr = "NetworkAddress";
+            }
+            //    Registry.LocalMachine.OpenSubKey(
+            //        @"SYSTEM\ControlSet001\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001", true);
+        }
+       
+
+        public RegistryKey GetWlanRegistryKey()
+        {
+            RegistryKey x = Registry.LocalMachine.OpenSubKey(
+                    @"SYSTEM\ControlSet001\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}", true);
+            List<string> subKeyNames = x.GetSubKeyNames().ToList();
+            foreach(string subKeyName in subKeyNames)
+            {
+                RegistryKey subkey = x.OpenSubKey(subKeyName, true);
+                List<string> keyValues = subkey.GetValueNames().ToList();
+                if (keyValues.Contains("NetType"))
+                    return subkey;
+            }
+            throw new InvalidOperationException();
+        }
+
+        public bool IsCustomMacKeyEnabled()
+        {
+            string netAddr = GetMacAddressFromRegistry();
+            if (netAddr != null)
+                return true;
+            return false;
+        }
+
+        public bool CreateCustomMacKey(string newAddress)
+        {
+            string oldNetAddr = GetMacAddressFromRegistry();
+            try
+            {
+                registryKey.SetValue(nwAdr, newAddress);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+               MessageBox.Show("Unable to modify registry - please run as an administrator!");
+            }
+            string newNetAddr = GetMacAddressFromRegistry();
+            if (newNetAddr != oldNetAddr)
+            {
+                if (newNetAddr == newAddress)
+                    return true;
+            }
+            return false;
+        }
+    
+
+        public bool DeleteCustomMacKey()
+        {
+            registryKey.DeleteSubKey(nwAdr);
+            string netAddr = GetMacAddressFromRegistry();
+            if (netAddr != null)
+                return false;
+            return true;
+        }
+
+        public bool ChangeCustomMacKey(string newMac)
+        {
+            registryKey.SetValue(nwAdr, newMac);
+            return true;
+        }
+
+        public string GetMacAddressFromRegistry()
+        {
+            return (string)registryKey.GetValue(nwAdr);
+            
+        }
+
+        ~RegistryManipulation()
+        {
+            registryKey.Close();
+            registryKey.Dispose();
+        }
+    }
+}
